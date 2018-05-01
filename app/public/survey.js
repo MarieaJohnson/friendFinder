373ca1e78c8d4d5d5d47dfedd2d42f4b95e043d5
Mariea Johnson <marieaajohnson@gmail.com>
2018-05-01 17:45:25 -0400

    updated files

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,1 +1,37 @@
 console.log("sanity");
+
+{/* <script type="text/javascript">
+
+  $(".submit").on("click", function(event) {
+    event.preventDefault();
+
+  var newReservation = {
+    name: $("#name").val().trim(),
+  phoneNumber: $("#reserve-phone").val().trim(),
+  customerEmail: $("#reserve-email").val().trim(),
+  customerID: $("#reserve-unique-id").val().trim()
+};
+
+console.log(newFriend);
+
+$.post("/api/Routes", newFriend,
+        function(data) {
+
+          if (data) {
+    alert("Yay! You are officially booked!");
+  }
+          else {
+    alert("Sorry you are on the wait list");
+  }
+
+  // Clear the form when submitting
+  $("#reserve-name").val("");
+  $("#reserve-phone").val("");
+  $("#reserve-email").val("");
+  $("#reserve-unique-id").val("");
+
+});
+
+});
+
+</script> */}
